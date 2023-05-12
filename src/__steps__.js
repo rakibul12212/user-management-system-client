ce78b428c4ddfd1df01955286cdffee437c9dd44
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-05-12 16:40:21 +0600

    first commit

---
+++ src/__steps__.js
@@ -0,0 +1,11 @@
+/**
+ * create a post api on the server side
+ * client side send data via post
+ * set fetch method inside the fetch options (second perameter)
+ * options will have three things : method
+ * option will have headers : 'content-type' :'application/json'
+ * don't forget to send data by JSON.Stringify in body:
+ * on the server side: don't forget to use express.json() middleware
+ * 
+ * 
+ * */ 
